d92e71e5ad4bc4a882d4d16f64fbffb2fcc3abec
Ruzanna <ruzanna.mehrab@gmail.com>
2019-07-05 19:30:10 +0400

    Added some functionality for slider Class / aading conditions

--- vimslider.js
+++ vimslider.js
@@ -10,9 +10,32 @@
 
     var slider;
 
-    var defOptions = {
+    var defaults = {
         slideShow: true,
         interval: 500,
         animation: 'slide'
     };
+
+    $.fn.vimslider = function (options) {
+
+        if (this.length === 0) {
+            return this;
+        }
+
+        if (this.length > 1) {
+            this.each(function () {
+                $(this).vimslider(options);
+            });
+            return this;
+        }
+
+        // Join User Options with default options
+        var settings = $.extend({}, defaults, options);
+
+        return this;
+    }
+
+    function slider() {
+
+    }
 });
